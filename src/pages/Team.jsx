8e5d47dab5023ace0a67d4ef6c82fced4e6c19c4
nikunjrana3186 <nikrana3186@gmail.com>
2026-06-05 10:16:00 +0530

    team done

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -297,6 +297,14 @@ export const teamMembers = [
     bio: "Passionate about developing performant web applications.",
     image: "https://www.shambhavitechnovation.com/assets/images/nitul.jpg",
     socials: { linkedin: "#", github: "#", twitter: "#" }
+  },
+  {
+    id: 20,
+    name: "Jay Prajapati",
+    role: "MERN Developer",
+    bio: "Focused on creating responsive and scalable web applications.",
+    image: "https://www.shambhavitechnovation.com/assets/images/jay-1.jpg",
+    socials: { linkedin: "#", github: "#", twitter: "#" }
   }
 ];
 
@@ -351,8 +359,9 @@ const Team = () => {
                 <img 
                   src={member.image} 
                   alt={member.name} 
-                  className="w-full h-full object-cover object-top group-hover:scale-105 transition-all duration-700 ease-in-out"
+                  className="w-full h-full object-contain  group-hover:scale-105 transition-all duration-700 ease-in-out"
                 />
+                {/* object-cover object-top */}
                 <div className="absolute inset-0 bg-gradient-to-t from-black/40 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
               </div>
 
